3d33e052ff23655391816aa47bc6488a43f0504d
Cody Rush <codyrush@gmail.com>
2019-03-24 18:32:48 -0700

    updated to use config.js

--- config/config.js
+++ config/config.js
@@ -9,15 +9,15 @@ module.exports = {
     dialect: "mysql"
   },
   test: {
-    username: "root",
-    password: null,
-    database: "testdb",
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASS,
+    database: process.env.MYSQL_DB,
     host: "localhost",
     dialect: "mysql",
     logging: false
   },
   production: {
-    use_env_variable: "JAWSDB_URL",
+    use_env_variable: process.env.JAWS_DB_URL,
     dialect: "mysql"
   }
 };
